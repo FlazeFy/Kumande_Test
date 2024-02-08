2e45411472cdfa44c58a4692e6512f1e7339a66b
Leonardho R Sitanggang <79736302+FlazeFy@users.noreply.github.com>
2024-02-08 08:19:22 +0700

    4 api test
    get total consume by from, total consume by type, total consume by main ing, total consume by provide

---
+++ support/integrations/get_api/get_stats.js
@@ -0,0 +1,62 @@
+// Components
+import { generateAuthToken } from '../../components/generate'
+import '../../components/template'
+
+describe('Kumande API Testing - Stats', () => {
+    // Template
+    const is_paginate = false
+    const method = 'get'
+    const token = generateAuthToken("hardcode")
+
+    it(method.toUpperCase() + ' - Total Consume By From', () => {
+        cy.request({
+            method: method,
+            url: '/api/v1/consume/total/byfrom',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as(method + 'TotalConsumeByFrom')
+        cy.get('@' + method + 'TotalConsumeByFrom').then(stats => {
+            cy.templateGet(stats, is_paginate)
+        })
+    })
+
+    it(method.toUpperCase() + ' - Total Consume By Type', () => {
+        cy.request({
+            method: method,
+            url: '/api/v1/consume/total/bytype',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as(method + 'TotalConsumeByType')
+        cy.get('@' + method + 'TotalConsumeByType').then(stats => {
+            cy.templateGet(stats, is_paginate)
+        })
+    })
+
+    it(method.toUpperCase() + ' - Total Consume By Main Ingredient', () => {
+        cy.request({
+            method: method,
+            url: '/api/v1/consume/total/bymain',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as(method + 'TotalConsumeByMain')
+        cy.get('@' + method + 'TotalConsumeByMain').then(stats => {
+            cy.templateGet(stats, is_paginate)
+        })
+    })
+
+    it(method.toUpperCase() + ' - Total Consume By Provide', () => {
+        cy.request({
+            method: method,
+            url: '/api/v1/consume/total/byprovide',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).as(method + 'TotalConsumeByProvide')
+        cy.get('@' + method + 'TotalConsumeByProvide').then(stats => {
+            cy.templateGet(stats, is_paginate)
+        })
+    })
+})
